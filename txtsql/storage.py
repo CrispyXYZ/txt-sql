301b3291750510602a214abe4f38e602d0297ffb
CrispyXYZ <64761703+CrispyXYZ@users.noreply.github.com>
2026-02-14 14:48:42 +0800

    Table creating and inserting

--- txtsql/storage.py
+++ txtsql/storage.py
@@ -1,7 +1,6 @@
 import csv
 import logging
 import os
-from pathlib import Path
 
 from txtsql.exceptions import TableAlreadyExistsError
 from txtsql.types import Types
@@ -12,25 +11,56 @@ metadata_filename = 'metadata.txt'
 
 def create_table(name: str, defs: dict[str, Types]) -> Table:
     logger.debug(f'Creating table: {name}')
-
-    if not os.path.exists(metadata_filename): \
-            open(metadata_filename, 'w', encoding='utf-8').close()
-
-    with open(metadata_filename, 'r', encoding='utf-8') as metadata:
-        reader = csv.reader(metadata, delimiter='\t')
-        for line in reader:
-            if line[0] == name:
+    try:
+        with open(metadata_filename, 'r', encoding='utf-8') as metadata:
+            reader = csv.reader(metadata, delimiter='\t')
+            if any(row[0] == name for row in reader):
                 raise TableAlreadyExistsError(f'Table already exists: {name}')
-    with open(metadata_filename, 'a') as metadata:
-        row: list[str] = [name, str(len(defs))]
-        row += [str(item) for pair in defs.items() for item in pair]
+    except FileNotFoundError:
+        pass
+
+    with open(metadata_filename, 'a', encoding='utf-8') as metadata:
+        row = [name, str(len(defs))] + [str(x) for pair in defs.items() for x in pair]
         logger.debug(f'Writing: {row}')
-        metadata.write('\t'.join(row))
-    return Table(name, defs)
+        metadata.write('\t'.join(row) + '\n')
+        return Table(name, defs)
+
+
+def get_table(name: str) -> Table | None:
+    logger.debug(f'Getting table: {name}')
+    try:
+        with open(metadata_filename, 'r', encoding='utf-8') as metadata:
+            reader = csv.reader(metadata, delimiter='\t')
+            row = next((row for row in reader if row[0] == name), None)
+            if row is None:
+                return None
+            count = int(row[1])
+            return Table(name, {row[i]: Types(row[i + 1]) for i in range(2, 2 + count * 2, 2)})
+    except FileNotFoundError:
+        logger.warning('metadata.txt file not found. Returning None.')
+        return None
 
 
 class Table:
+    name: str
+    filename: str
+    defs: dict[str, Types]
+
     def __init__(self, name: str, defs: dict[str, Types]) -> None:
         self.name = name
-        self.path = Path(name)
+        self.filename = f'{self.name}.txt'
         self.defs = defs
+
+    def ensure_file(self) -> None:
+        if not os.path.exists(self.filename):
+            logger.debug(f'Creating file: {self.filename}')
+            open(self.filename, 'w', encoding='utf-8').close()
+
+    def insert_values(self, values: dict[str, str]) -> None:
+        """ This method does not accept None in values parameter. """
+        logger.debug(f'Inserting values: {values}')
+        with open(self.filename, 'a', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file, delimiter='\t')
+            row = [values.get(key) for key in self.defs]
+            logger.debug(f'Writing: {row}')
+            writer.writerow(row)
