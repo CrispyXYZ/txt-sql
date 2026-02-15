24e8c93c144515bf8d59738665c866b2c558d9c9
CrispyXYZ <64761703+CrispyXYZ@users.noreply.github.com>
2026-02-15 18:48:12 +0800

    Table updating

--- txtsql/__main__.py
+++ txtsql/__main__.py
@@ -6,12 +6,14 @@ from txtsql.types import Types
 
 def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
+
     if storage.get_table('users') is None:
         storage.create_table('users', {'name': Types.STRING, 'age': Types.NUMBER})
     if storage.get_table('foo') is None:
         storage.create_table('foo', {'name': Types.STRING, 'age': Types.NUMBER})
     users = storage.get_table('users')
-    users.insert_values({'name': 'John', 'age': 24})
+    # users.insert_values({'name': 'John', 'age': 24})
+    users.update({'age': '22'})
 
 
 if __name__ == '__main__':
--- txtsql/storage.py
+++ txtsql/storage.py
@@ -1,6 +1,7 @@
 import csv
 import logging
 import os
+from collections.abc import Callable
 
 from txtsql.exceptions import TableAlreadyExistsError
 from txtsql.types import Types
@@ -23,7 +24,9 @@ def create_table(name: str, defs: dict[str, Types]) -> Table:
         row = [name, str(len(defs))] + [str(x) for pair in defs.items() for x in pair]
         logger.debug(f'Writing: {row}')
         metadata.write('\t'.join(row) + '\n')
-        return Table(name, defs)
+        table: Table = Table(name, defs)
+        table.ensure_file()
+        return table
 
 
 def get_table(name: str) -> Table | None:
@@ -57,10 +60,42 @@ class Table:
             open(self.filename, 'w', encoding='utf-8').close()
 
     def insert_values(self, values: dict[str, str]) -> None:
-        """ This method does not accept None in values parameter. """
+        """ This method does not accept None in values parameter. Use empty string instead. """
         logger.debug(f'Inserting values: {values}')
         with open(self.filename, 'a', encoding='utf-8', newline='') as file:
             writer = csv.writer(file, delimiter='\t')
             row = [values.get(key) for key in self.defs]
             logger.debug(f'Writing: {row}')
             writer.writerow(row)
+
+    def update(self, values: dict[str, str], where: Callable[[dict[str, str]], bool] | None = None) -> None:
+        """ This method does not accept None in values parameter. Use empty string instead.
+            :param values: Dictionary of values to update
+            :param where: Function which takes a row dictionary as parameter
+        """
+        logger.debug(f'Updating values: {values}')
+
+        def_keys: list[str] = list(self.defs.keys())
+        def_count: int = len(def_keys)
+        updated_values: list[str | None] = [values.get(col) if col in list(values.keys()) else None for col in def_keys]
+        logger.debug(f'Generated values: {updated_values}')
+
+        if all(value is None for value in updated_values):
+            logger.warning(f'No values to update: {values}')
+            return
+
+        table_values: list[list[str]] = []
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter='\t')
+            for row in reader:
+                if where is None or where({def_keys[i]: row[i] for i in range(def_count)}):
+                    new_row = [row[i] if updated_values[i] is None else updated_values[i] for i in range(def_count)]
+                else:
+                    new_row = row
+                table_values.append(new_row)
+
+        logger.debug(f'Updated values: {table_values}')
+
+        with open(self.filename, 'w', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file, delimiter='\t')
+            writer.writerows(table_values)
